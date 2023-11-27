a474a0ef22815711d498a91769fb107a982297b5
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2023-11-27 23:45:47 +0400

    learn process object in node.js that's give us info about node.js processes

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 console.log("start learning");
 console.log("continue learning node.js");
+
+process.exit(3); //This syntax is the correct way to use the process.exit method.
+// It exits the Node.js process with the specified exit code (in this case, 3).
+process.exit = 3; // This syntax is assigning the value 3 to the exit property of the process object,
+//  which is not the intended use of process.exit.
