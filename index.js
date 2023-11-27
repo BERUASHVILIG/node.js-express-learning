94f7b210e1277e81029712b15365873a501cb1d2
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2023-11-27 23:46:49 +0400

    add desc for process object in node.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 console.log("start learning");
-console.log("continue learning node.js");
+console.log("continue learning node.j");
 
 process.exit(3); //This syntax is the correct way to use the process.exit method.
 // It exits the Node.js process with the specified exit code (in this case, 3).
 process.exit = 3; // This syntax is assigning the value 3 to the exit property of the process object,
 //  which is not the intended use of process.exit.
+
+// The process object is a global object in Node.js that provides information about the current Node.js process.
+// It allows access to various properties and methods related to the process, such as environment variables,
+// command-line arguments, and process-related events.
