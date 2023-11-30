7add678e457d1725ea26f3eb5bd4474a67340c21
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2023-11-30 23:40:42 +0400

    learn about input in terminal, also lear promp-sync package that makes more simple inputs

---
+++ input.js
@@ -0,0 +1,20 @@
+const readLine = require("readline");
+const prompt = require("prompt-sync")();
+
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// using read line can make question in terminal and then using callback function can back answer
+
+// rl.question("What is your name?", (name) => {
+//   console.log(`hi${name}`);
+//   rl.close();
+// });
+
+// if dont want to use callback function exist "prompt-sync" package which can make same thing
+
+const name = prompt("What is your name?");
+console.log(`Hi ${name}`);
+rl.close();
