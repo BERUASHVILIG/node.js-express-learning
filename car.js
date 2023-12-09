9b51b894beb570fe066b6dd25670d307db08b0c4
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2023-12-09 15:57:38 +0400

    learn how to export and import objects in different way

---
+++ car.js
@@ -0,0 +1,13 @@
+exports.car1 = {
+  branch: "Ford",
+  model: "Fiesta",
+};
+
+exports.car2 = {
+  branch: "Tesla",
+  model: "model 3",
+};
+
+// module.exports = car;
+
+// exports.data = { car1, car2 };
--- index.js
+++ index.js
@@ -10,14 +10,32 @@
 // It allows access to various properties and methods related to the process, such as environment variables,
 // command-line arguments, and process-related events.
 
-const _ = require("lodash");
+// const _ = require("lodash");
 
-const arr = [1, 3, 5, 32];
+// const arr = [1, 3, 5, 32];
 
 // console.log(_.chunk(arr)); // [ [ 1 ], [ 3 ], [ 5 ], [ 32 ] ]
 
-console.log(_.chunk(arr));
+// console.log(_.chunk(arr));
 
 // lodash is a utility library that provides many helpful functions for working with JavaScript data structures,
 //  including arrays and objects. It simplifies common programming tasks and helps to avoid reinventing the wheel.
 // / Some of the features provided by lodash include:
+
+// const cowsay = require("cowsay");
+
+// console.log(
+//   cowsay.say({
+//     text: "I am learning npm module",
+//     e: "00",
+//     T: "U",
+//   })
+// );
+
+// import exported data
+// const car = require("./car");
+// const { data } = require("./car");
+const { car1, car2 } = require("./car");
+
+console.log(JSON.stringify(car1, null, 2));
+console.log(JSON.stringify(car2, undefined, 2));
