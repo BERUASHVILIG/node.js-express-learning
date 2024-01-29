f53e0d03e29e9f283df41c46a39a18612885991d
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2024-01-29 17:03:36 +0400

    start making CRUD Api using vanilla - createserver, make data and method files

---
+++ Create-Api-CRUD-Project/server.js
@@ -0,0 +1,32 @@
+const http = require("http");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5001;
+
+const server = http.createServer((req, res) => {
+  switch (req.method) {
+    case "GET":
+      getReq(req, res);
+      break;
+    case "POST":
+      postReq(req, res);
+      break;
+    case "PUT":
+      putReq(req, res);
+      break;
+    case "DELETE":
+      deleteReq(req, res);
+      break;
+    default:
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "application/json");
+      res.write(
+        JSON.stringify({ title: "NOT Found", message: "Route not found" })
+      );
+      res.end();
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
