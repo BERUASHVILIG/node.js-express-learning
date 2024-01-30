386e1189b2685b504653abb3dc15948d5b05f9ce
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2024-01-30 17:08:04 +0400

    create method files and create get-request

--- Create-Api-CRUD-Project/server.js
+++ Create-Api-CRUD-Project/server.js
@@ -1,9 +1,17 @@
 const http = require("http");
+const getReq = require("./methods/get-request");
+const postReq = require("./methods/post-request");
+const putReq = require("./methods/put-request");
+const deleteReq = require("./methods/delete-request");
+
+let movies = require("./data/movies.json");
+
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5001;
 
 const server = http.createServer((req, res) => {
+  req.movies = movies;
   switch (req.method) {
     case "GET":
       getReq(req, res);
