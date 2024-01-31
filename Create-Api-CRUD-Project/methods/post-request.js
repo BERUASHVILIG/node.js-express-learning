fad2729d66cd186a0af1c5f81ea6f14013595238
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2024-01-31 17:12:59 +0400

    make post-request write in file to update database, write file is database changer

--- Create-Api-CRUD-Project/methods/post-request.js
+++ Create-Api-CRUD-Project/methods/post-request.js
@@ -1,1 +1,28 @@
-module.exports = (req, res) => {};
+const crypto = require("crypto");
+const requestBodyParser = require("../util/body-parser");
+const writeToFile = require("../util/write-to-file");
+module.exports = async (req, res) => {
+  if (req.url === "/api/movies") {
+    try {
+      let body = await requestBodyParser(req);
+      body.id = crypto.randomUUID();
+      req.movies.push(body);
+      writeToFile(req.movies);
+      res.writeHead(201, { "Content-Type": "application/json" });
+      res.end();
+      console.log("Request body: ", body);
+    } catch (error) {
+      console.log(error);
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({
+          title: "Validation Failed",
+          message: "Request body is not valid",
+        })
+      );
+    }
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ title: "NOT Found", message: "Route not found" }));
+  }
+};
---
+++ Create-Api-CRUD-Project/util/write-to-file.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = (data) => {
+  try {
+    fs.writeFileSync(
+      path.join(__dirname, "..", "data", "movies.json"),
+      JSON.stringify(data),
+      "utf-8"
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
