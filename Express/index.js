2f9580e11edfd6f9f72fcff16115c400e4977de6
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2024-02-01 17:38:55 +0400

    make basic Routing in Express

--- Express/index.js
+++ Express/index.js
@@ -6,7 +6,27 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
-  res.json({ message: "hello world" });
+  res.json({ message: "This is Home Page" });
+});
+
+app.get("/users", (req, res) => {
+  res.json({ message: "Get all the users" });
+});
+
+app.get("/users/:id", (req, res) => {
+  res.json({ message: `Get user with ID ${req.params.id}` });
+});
+
+app.post("/users/", (req, res) => {
+  res.json({ message: `Create new User` });
+});
+
+app.put("/users/:id", (req, res) => {
+  res.json({ message: `Update User with ID ${req.params.id}` });
+});
+
+app.delete("/users/:id", (req, res) => {
+  res.json({ message: `Delete User with ID ${req.params.id}` });
 });
 
 app.listen(PORT, () => {
