36afba78da30352772a9e29aa7b5448f0f9fa924
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2024-02-03 15:16:13 +0400

    error handler middleware using on appliaction level middleware

--- Express-middlware-types/index.js
+++ Express-middlware-types/index.js
@@ -41,6 +41,27 @@ router.route("/").get(getUsers).post(createUser);
 // Built-in Middleware
 // Error-handling middleware
 
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  res.status(statusCode);
+  switch (statusCode) {
+    case 401:
+      res.json({ title: "Unauthorized", message: err.message });
+      break;
+    case 404:
+      res.json({ title: "Not Found", message: err.message });
+      break;
+    case 500:
+      res.json({ title: "Server Error", message: err.message });
+      break;
+
+    default:
+      break;
+  }
+};
+
+app.use(errorHandler);
+
 app.listen(PORT, () => {
   console.log(`Server listen on port ${PORT}`);
 });
